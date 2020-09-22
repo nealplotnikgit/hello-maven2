66f79aec3ab50f0c76a9085b9974484fdebff927
Neal Plotnik <nealplotnik@gmail.com>
2020-09-21 20:59:48 -0500

    Initial commit

---
+++ src/main/java/academy/learnprogramming/HelloMaven.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMaven {
+    private static final Logger log = LoggerFactory.getLogger(HelloMaven.class);
+    public static void main(String[] args) {
+        //System.out.println("Hello Maven");
+        log.info("Guess-The-Number-game");
+        //https://dev.to/techgirl1908/intellij-error-java-release-version-5-not-supported-376
+    }
+}
